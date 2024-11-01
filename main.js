2536ee65694329b0510f09e45e7ed00eddd89c2e
BindBoy <emilioj_es@hotmail.com>
2024-11-01 13:31:47 +0100

    Tarea DWEC 01

---
+++ main.js
@@ -0,0 +1,5 @@
+// Declaramos una constante asignándole una función anónima que muestra un mensaje de alerta
+const saludos = () => {
+    alert('Hola, compañeros de DAW');
+}
+
---
+++ node.js
@@ -0,0 +1,9 @@
+//Definimos una función que imprime un mensaje en la consola
+// En consola no podemos usar alert, por lo que usamos console.log
+
+function saludos() {
+
+        console.log("Hola, compañeros de DAW");
+}
+
+saludos();
